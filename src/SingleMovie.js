5d3824af6d919a1733606da00a833c822c8e7cdc
Prakhar <prakhardubey2002@gmail.com>
2022-07-15 15:30:26 +0530

    React router added, path intialised,error page added

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import {useParams} from 'react-router-dom';
 const SingleMovie = () => {
+  const {id}= useParams();
   return (
-    <div>SingleMovie</div>
-  )
-}
+    <>
+    <div>SingleMovie id : {id}</div>
+    </>
+  );
+};
 
 export default SingleMovie
