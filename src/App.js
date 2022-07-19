da6eee76bcd790a51efb744b651942246f9a557c
Prakhar <prakhardubey2002@gmail.com>
2022-07-19 22:21:30 +0530

    api mapped,css added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './Home';
 import SingleMovie from './SingleMovie'
 import Error  from './Error';
+import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 const App = () => {
   return (
--- src/Movies.js
+++ src/Movies.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import { useGlobalContext } from './context';
 
 const Movies = () => {
- const {movie}=useGlobalContext();
+  const { movie } = useGlobalContext();
   return (
     <>
-    <div>Movies</div>
-    {movie.map((curmovie)=>{
-      
-      return  <p key={curmovie.imdbID} >  {curmovie.Title}</p>
-    })}
+      <section className="movie-page">
+        <div className="container grid grid-4-col">
+          {movie.map((curMovie) => { 
+            const {imdbID,Title,Poster}=curMovie;//filed are availbale in api
+            return (
+                    <NavLink key={imdbID} to={`movie/${imdbID}`}>
+                          <div  className="card">
+                            <div className="card-info">
+                              <h2>{Title}</h2>
+                              <img src={Poster} alt={imdbID} />
+                            </div>
+                          </div>
+                    </NavLink>
+                   )
+          })}
+        </div>
+      </section>
     </>
   )
 }
